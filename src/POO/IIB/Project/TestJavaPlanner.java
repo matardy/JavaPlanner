2d7dee4f0623cb1224ed50d4046e6795036e3e0d
Gutemberg S. Mendoza <gutemberg.mendoza@epn.edu.ec>
2021-09-03 11:47:47 -0500

    Main para test.

---
+++ src/POO/IIB/Project/TestJavaPlanner.java
@@ -0,0 +1,17 @@
+
+package POO.IIB.Project;
+
+/**
+ *
+ * @author StevG
+ */
+public class TestJavaPlanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Espacio para probar nuevas clases creadas. 
+    }
+    
+}
