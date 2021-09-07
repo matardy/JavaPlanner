9be9cdfd2281947e1c2fc14433a267615269da9e
Gutemberg S. Mendoza <gutemberg.mendoza@epn.edu.ec>
2021-09-07 15:02:39 -0500

    Se crearon variables de instancia de la clase evento y el constructor.

--- src/POO/IIB/Project/Evento.java
+++ src/POO/IIB/Project/Evento.java
@@ -6,7 +6,15 @@ package POO.IIB.Project;
  * @author StevG
  */
 public class Evento {
+    //Falta gestionar ingresar las horas
     protected String nombre; 
+    protected String descripcion;
+    protected String detalles; 
     
+    public Evento(String nombre, String descripcion, String detalles){
+        this.nombre= nombre; 
+        this.descripcion = descripcion; 
+        this.detalles = detalles; 
+    }
     //Aqui toca añadir los diferentes tipos de datos. 
 }
