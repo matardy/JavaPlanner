10637cbb4355194fabfe1d5bb61223a9acb70d38
IGRA27 <isaac.reyes@epn.edu.ec>
2021-09-13 15:15:00 -0500

    Adaptacion del Mensaje de error con uso de validador, tanto en clase como fuente y GUI, con clase Validador

---
+++ src/POO/IIB/Project/Validador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POO.IIB.Project;
+
+/**
+ *
+ * @author Isaac
+ */
+//CREAMOS VALIDADOR CON EXPRESIONES REGULARES DE LAS 3 PARTES DE LA GUI PARA USUARIO
+public class Validador {
+
+   public static boolean verificarNombre(String nombre){
+        return nombre.matches("^[a-zA-ZñÑ]+$");
+    }
+   public static boolean verificarDescripcion(String descripcion){
+       return descripcion.matches("^[a-zA-ZñÑ]+$");
+   }
+    public static boolean verificarDetalles(String detalles){
+        return detalles.matches("^[a-zA-ZñÑ]+$");
+    }
+}
